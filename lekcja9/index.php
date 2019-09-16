6330a6899ae2e435d3e6bf86041d358fb316bcdf
4tieko <3ti06eko2018@gmail.com>
2019-09-16 09:07:26 +0200

    lekcja10

--- lekcja9/index.php
+++ lekcja9/index.php
@@ -1,6 +1,6 @@
 <?php
 
-    $liczba=1;
+    $liczba=-1;
 
     // if($liczba<0){
     //     echo $liczba;
@@ -12,11 +12,38 @@
     // }
 
     
-    if($liczba>=0){
-        echo "Liczba jest równa lub więszka od zera!";
-    }else{
-        echo $liczba;
-        echo "<br/>Liczba jest mniejsza od zera!";
-    }
+    // if($liczba>=0){
+    //     echo "Liczba jest równa lub więszka od zera!";
+    // }else{
+    //     echo $liczba;
+    //     echo "<br/>Liczba jest mniejsza od zera!";
+    // }
 
+
+    // if($liczba>0){
+    //     echo "Liczba jest więszka od zera!";
+    // }else if($liczba==0){
+    //     echo "Liczba jest równa zero!";
+    // }else{
+    //     echo "Liczba jest mniejsza od zera!";
+    // }
+
+    $a=-3;
+    $b=4;
+    $c=1;
+
+    $delta = ($b*$b)-4*$a*$c;
+    
+    if($a==0){
+        echo "Równanie nie jest kwadratowe!";
+    }else if($delta>0){
+        $x1 = (-($b)+sqrt($delta))/2*$a;
+        $x2 = (-($b)-sqrt($delta))/2*$a;
+        echo "x<sub>1</sub>=$x1</br>";
+        echo "x<sub>2</sub>=$x2";
+    }else if($delta==0){
+        $x = (-($b))/2*$a;
+        echo "Delta jest równa 0!";
+        echo  "x=$x";
+    }
 ?>

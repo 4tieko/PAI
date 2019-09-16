63f936c4994e106deebbf925e60b6086a6648646
4tieko <3ti06eko2018@gmail.com>
2019-09-16 08:34:53 +0200

    lekcja9

--- lekcja5/index.php
+++ lekcja5/index.php
@@ -1,6 +1,5 @@
 <?php
-
     include "plikasd.php";
     require "plikasd.php";
-
+    
 ?>
---
+++ lekcja9/index.php
@@ -0,0 +1,22 @@
+<?php
+
+    $liczba=1;
+
+    // if($liczba<0){
+    //     echo $liczba;
+    //     echo "<br/>Liczba jest mniejsza od zera!";
+    // }
+
+    // if($liczba>=0){
+    //     echo "Liczba jest równa lub więszka od zera!";
+    // }
+
+    
+    if($liczba>=0){
+        echo "Liczba jest równa lub więszka od zera!";
+    }else{
+        echo $liczba;
+        echo "<br/>Liczba jest mniejsza od zera!";
+    }
+
+?>

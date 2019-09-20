c4c58bb496282111c16b4066aa5f5031d4c4d119
4tieko <3ti06eko2018@gmail.com>
2019-09-20 08:41:32 +0200

    lekcja11

---
+++ lekcja11/data.php
@@ -0,0 +1,15 @@
+<?php
+
+    $arr = array(
+        1=>$_POST['imie'],
+        2=>$_POST['nazwisko'],
+        3=>$_POST['wiek'],
+        4=>$_POST['wzrost'],
+        5=>$_POST['waga']
+    );
+
+    foreach ($arr as $key => $value) {
+        echo "$value </br>";
+    }
+
+?>
--- lekcja11/index.php
+++ lekcja11/index.php
@@ -1,5 +1,19 @@
-<?php
-
-    
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="data.php" autocomplete="off" method="post">
+        <input type="text" placeholder="imie" name="imie">
+        <input type="text" placeholder="nazwisko" name="nazwisko">
+        <input type="text" placeholder="wiek" name="wiek">
+        <input type="text" placeholder="wzrost" name="wzrost">
+        <input type="text" placeholder="waga" name="waga">
+        <input type="submit" value="Dodaj">
+    </form>
+</body>
+</html>

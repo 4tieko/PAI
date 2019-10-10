d3e9d8a481f7ddb8c01b1facd5a4a896d7b689cf
4tieko <3ti06eko2018@gmail.com>
2019-10-10 18:38:53 +0200

    cos na 5

--- lekcja25/index.php
+++ lekcja25/index.php
@@ -1,5 +1,10 @@
 <?php
-
-    echo date('o-m-d e')
-
+        for($i=1;$i<=3;$i++){
+            for($y=1;$y<=3;$y++){
+                echo "$i$y&nbsp&nbsp&nbsp";
+                if($y==3){
+                    echo "</br>";
+                }
+            }
+        }
 ?>
